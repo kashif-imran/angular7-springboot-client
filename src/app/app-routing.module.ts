22f98e4a54c0d4b42ac156fbc195595a6d076fa5
Kashif Imran <kashif_joseph11@yahoo.com>
2019-04-02 14:59:07 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'add', component: CreateEmployeeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
